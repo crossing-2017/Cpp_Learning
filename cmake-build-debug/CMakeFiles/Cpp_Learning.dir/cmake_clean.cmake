ecdbbd04a3784b3dd5c5c866d426abd9cbb0c148
crossing-2017 <crossing_2017@163.com>
2019-05-01 11:47:43 +0800

    用CLion写的第一个Cpp程序

---
+++ cmake-build-debug/CMakeFiles/Cpp_Learning.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_Learning.dir/main.cpp.o"
+  "Cpp_Learning.pdb"
+  "Cpp_Learning"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_Learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
