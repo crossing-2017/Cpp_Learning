ea7c1c5d2cee776da6bae6354cb18f5eea723428
crossing-2017 <crossing_2017@163.com>
2019-05-01 10:55:32 +0800

    Initial Commit

---
+++ Cpp_Learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp_Learning
+//
+//  Created by Crossing on 2019/5/1.
+//  Copyright © 2019 Crossing. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
